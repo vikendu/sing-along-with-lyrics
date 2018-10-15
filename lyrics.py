3feb31552950b9f3b578b72f8dbd54572fc6bb9f
Vikendu Singh <vikendu@gmail.com>
2018-10-15 13:28:05 +0530

    404 exception handled. User input to exit loop.

--- lyrics.py
+++ lyrics.py
@@ -1,31 +1,39 @@
 import requests, bs4, sys
 
-artist=input("Enter Artist's Name: ").replace(" ","")
-song=input("Track Name: ").replace(" ","")#azlyrics doesnot take spaces
+while(True):
+	print('Press Y to continue or N to exit:')
+	if(input('') == 'n' or input('') == 'N'):
+		sys.exit()
+	else:
 
-# url layout: www.azlyrics.com/lyrics/<artist>/<song>.html
-url='http://www.azlyrics.com/lyrics/%s/%s.html' % (artist,song)
+		artist=input("Enter Artist's Name: ").replace(" ","")
+		song=input("Track Name: ").replace(" ","")#azlyrics doesnot take spaces
 
-#to bypass azlyrics' DDoS protection, using headers it believes the program is a browser
-#some sample header on Linux and Windows
-#'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' 
-#'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0
-#FROM MY MACHINE:
-#'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+		# url layout: www.azlyrics.com/lyrics/<artist>/<song>.html
+		url='http://www.azlyrics.com/lyrics/%s/%s.html' % (artist,song)
 
-headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36' }
+		#to bypass azlyrics' DDoS protection, using headers it believes the program is a browser
+		#some sample header on Linux and Windows
+		#'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' 
+		#'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0
+		#FROM MY MACHINE:
+		#'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+
+		headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36' }
 
 
-res=requests.get(url,headers=headers)
-try:
-  res.raise_for_status()
-except Exception as exc:
-  print(exc) # print error
-  sys.exit() # end program on error
+		res=requests.get(url,headers=headers)
+		try:
+			res.raise_for_status()
+		except Exception as exc:
+			print(exc) # print error
+			#sys.exit() 
+			continue
 
-print('\nConnection %s' % url)
+		print('\nConnecting to: %s' % url)
 
-site=bs4.BeautifulSoup(res.text,"html.parser")
+		site=bs4.BeautifulSoup(res.text,"html.parser")
 
-for lyrics in site.find_all("div", {"class":""}):
-  print(lyrics.text)
+		for lyrics in site.find_all("div", {"class":""}):
+			print(lyrics.text)
+
