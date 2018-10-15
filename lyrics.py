cdec59ad5342a6f06f66a993fe62d2abaaac3fef
Vikendu Singh <vikendu@gmail.com>
2018-10-15 20:19:16 +0530

    few tweaks

--- lyrics.py
+++ lyrics.py
@@ -1,8 +1,18 @@
 import requests, bs4, sys
 
+#to bypass azlyrics' DDoS protection, using headers it believes the program is a browser
+#some sample header on Linux and Windows
+#'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' 
+#'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0
+#FROM MY MACHINE:
+#'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+
+headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36' }
+
 while(True):
 	print('Press Y to continue or N to exit:')
-	if(input('') == 'n' or input('') == 'N'):
+	choice = input('')
+	if(choice == 'n' or choice == 'N'):
 		sys.exit()
 	else:
 
@@ -11,15 +21,6 @@ while(True):
 
 		# url layout: www.azlyrics.com/lyrics/<artist>/<song>.html
 		url='http://www.azlyrics.com/lyrics/%s/%s.html' % (artist,song)
-
-		#to bypass azlyrics' DDoS protection, using headers it believes the program is a browser
-		#some sample header on Linux and Windows
-		#'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' 
-		#'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0
-		#FROM MY MACHINE:
-		#'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-
-		headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36' }
 
 
 		res=requests.get(url,headers=headers)
